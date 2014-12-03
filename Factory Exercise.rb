f5a43ced9ed3f7baf6e490d097c93f2223b9b31b
ericchencn2014 <ericchencn@outlook.com>
2014-12-03 23:11:38 +0000

    Update Factory Exercise.rb

--- Factory Exercise.rb
+++ Factory Exercise.rb
@@ -3,27 +3,66 @@
 #
 
 require_relative 'factory'
+require_relative 'asbtract_factory.rb'
 
-class Client
-  attr_writer :type
-
-	def initialize(type)
-    @type = type
+class Client 
+	def initialize(product_type)
+    #@factory = Factory.get_instance
+    @factory = product_type
   end
 
 	def foo
-    @type.new.create_product_A.do_your_stuff
-    @type.new.create_product_B.do_it
-    @type.new.create_product_C.perform
+# Implementation for Question 4
+    @factory.create_prod_A.do_your_stuff
+    @factory.create_prod_B.do_it
+    @factory.create_prod_C.perform
+    @factory.create_prod_D.run
+# Implementation for Question 3
+=begin
+		@factory.create_prod_A.do_your_stuff
+    @factory.create_prod_B.do_it
+    @factory.create_prod_C.perform
+=end
 	end
 end
 
+# Implementation for Question 3
+=begin
+my_client1 = Client.new(CoolFactory.new)
+my_client1.foo
 
-my_client = Client.new(CoolFactory)
-my_client.foo
-
-my_client2 = Client.new(UnCoolFactory)
+my_client2 = Client.new(UnCoolFactory.new)
 my_client2.foo
+=end
 
-my_client3 = Client.new(DeadlyFactory)
+# Implementation for Question 4
+=begin
+my_client3 = Client.new(CoolFactory.new)
 my_client3.foo
+
+my_client4 = Client.new(UnCoolFactory.new)
+my_client4.foo
+
+my_client5 = Client.new(DeadlyFactory.new)
+my_client5.foo
+=end
+
+# Implementation for Question 6
+CoolFactory.get_coolfactory
+instance = CoolFactory.get_instance
+my_client5 = Client.new(instance)
+my_client5.foo
+
+UnCoolFactory.get_uncoolfactory
+instance = UnCoolFactory.get_instance
+my_client6 = Client.new(instance)
+my_client6.foo
+
+DeadlyFactory.get_deadlyfactory
+instance = DeadlyFactory.get_instance
+my_client6 = Client.new(instance)
+my_client6.foo
+
+#obj1 = CoolFactory.new
+#obj2 = UnCoolFactory.new
+#obj3 = DeadlyFactory.new

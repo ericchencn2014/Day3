410677909a06604fdd2c2bc06b18b799301abed6
ericchencn2014 <ericchencn@outlook.com>
2014-12-03 21:57:33 +0000

    Update Factory.rb

--- Factory.rb
+++ Factory.rb
@@ -1,4 +1,9 @@
+
+
 class Factory
+  def initialize
+    @value = 10
+  end
 
   def create_product_A
     return ConcreteFactory_A.new
@@ -11,25 +16,31 @@ class Factory
   def create_product_C
     return ConcreteFactory_C.new
   end
+
+  def self.get_instance
+    @instance ||= new
+  end
+
+  private_class_method :new
 end
 
 
 
-class ConcreteFactory_A < Factory
+class ConcreteFactory_A
    def do_your_stuff
     puts "I'm a ProductA, doing my stuff"
   end
 
 end
 
-class ConcreteFactory_B < Factory
+class ConcreteFactory_B
   def do_it
     puts "I'm a ProductB, doing it"
   end
 end
 
 
-class ConcreteFactory_C < Factory
+class ConcreteFactory_C
   def perform
     puts "I'm a ProductC, performing"
   end

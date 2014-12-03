32f58526f4473a0fef5472022242f955aaa784fd
ericchencn2014 <ericchencn@outlook.com>
2014-12-03 21:53:23 +0000

    Create Factory Exercise.rb

---
+++ Factory Exercise.rb
@@ -0,0 +1,22 @@
+#
+# ABSTRACT FACTORY EXERCISE
+#
+
+require_relative 'factory'
+
+class Client 
+	def initialize
+    @factory = Factory.new
+  end
+
+	def foo
+		@factory.create_product_A.do_your_stuff
+    @factory.create_product_B.do_it
+    @factory.create_product_C.perform
+	end
+end
+
+
+my_client = Client.new
+my_client.foo
+
